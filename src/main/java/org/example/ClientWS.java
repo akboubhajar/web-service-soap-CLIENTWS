1adab224ef2722c39582c7d559c8e90f32489c31
akboubhajar <hajarakboub@gmail.com>
2023-12-25 12:52:41 +0100

    2nd commit

---
+++ src/main/java/org/example/ClientWS.java
@@ -0,0 +1,26 @@
+package org.example;
+import proxy.BanqueService;
+import proxy.BanqueWS;
+import proxy.Compte;
+
+import java.util.List;
+
+public class ClientWS {
+    public static void main(String[] args){
+        BanqueService proxy = new BanqueWS().getBanqueServicePort();
+        System.out.println(proxy.conversionEuroToDH(800));
+        Compte cp = proxy.getCompte(5);
+        System.out.println("Code: "+cp.getCode());
+        System.out.println("Solde: "+cp.getSolde());
+        System.out.println("dDate Creation: "+cp.getDateCreation());
+        List<Compte> compteList = proxy.listComptes();
+        for (Compte c :  compteList){
+            System.out.println("-------------------");
+            System.out.println("Code: "+c.getCode());
+            System.out.println("Solde: "+c.getSolde());
+            System.out.println("dDate Creation: "+c.getDateCreation());
+        }
+
+    }
+
+}
